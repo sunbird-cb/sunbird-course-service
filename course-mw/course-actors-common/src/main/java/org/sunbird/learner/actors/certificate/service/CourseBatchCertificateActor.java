4a9eaf35f72b2e532583d4ec3d779e9e645a6f81
saipradeep_ravipati <saipradeep.ravipati@tarento.com>
2023-09-07 03:44:50 +0530

    Converting Batch Attributes String to Object while storing into Elastic Search

--- course-mw/course-actors-common/src/main/java/org/sunbird/learner/actors/certificate/service/CourseBatchCertificateActor.java
+++ course-mw/course-actors-common/src/main/java/org/sunbird/learner/actors/certificate/service/CourseBatchCertificateActor.java
@@ -11,6 +11,7 @@ import java.util.Map;
 import org.apache.commons.collections.MapUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.sunbird.actor.base.BaseActor;
+import org.sunbird.common.CassandraUtil;
 import org.sunbird.common.exception.ProjectCommonException;
 import org.sunbird.common.models.response.Response;
 import org.sunbird.common.models.util.JsonKey;
@@ -61,10 +62,8 @@ public class CourseBatchCertificateActor extends BaseActor {
     logger.info(request.getRequestContext(), "Validated certificate template to batchID: " +  batchId);
     courseBatchDao.addCertificateTemplateToCourseBatch(request.getRequestContext(), courseId, batchId, templateId, template);
     logger.info(request.getRequestContext(), "Added certificate template to batchID: " +  batchId);
-    //Map<String, Object> courseBatch =
-    //    mapESFieldsToObject(courseBatchDao.getCourseBatch(request.getRequestContext(), courseId, batchId));
     Map<String, Object> courseBatch =
-            mapESFieldsToObject(mapper.convertValue(courseBatchDao.readById((String) request.get(JsonKey.COURSE_ID), batchId, request.getRequestContext()), Map.class));
+        mapESFieldsToObject(CassandraUtil.convertStringToMap(courseBatchDao.getCourseBatch(request.getRequestContext(), courseId, batchId),"batchAttributes"));
     CourseBatchUtil.syncCourseBatchForeground(request.getRequestContext(), batchId, courseBatch);
     logger.info(request.getRequestContext(), "Synced to es certificate template to batchID: " +  batchId);
     Response response = new Response();
