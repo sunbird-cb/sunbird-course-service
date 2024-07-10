d3270d0cd651a16411c7585030a1fbffaa588fb4
tarentomaheshvakkund <139739142+tarentomaheshvakkund@users.noreply.github.com>
2024-07-10 14:20:04 +0530

    KB-5718 | DEV|Assessment |BE| Capturing the starttime and endtime of the Batches while creating the batch for Assessment. (#158)
    * KB-5718 | DEV|Assessment |BE| Capturing the starttime and endtime of the Batches while creating the batch for Assessment.
    
    1.Created a new variable primaryCategory to check if it is a standalone assessment.
    2. Added a new validation to check if its a standalone assessment primary category.
    3. So the new if case checks the entire timestamp from cassandra db .
    
    * KB-5718 | DEV|Assessment |BE| Capturing the starttime and endtime of the Batches while creating the batch for Assessment.
    
    1. Add a custom logic to check the enrollment date is future wrt current time stamp.
    
    * KB-5718 | DEV|Assessment |BE| Capturing the starttime and endtime of the Batches while creating the batch for Assessment.
    
    1. Added the logic for admin enrolling of user for program api also.

--- course-mw/sunbird-util/sunbird-platform-core/common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
+++ course-mw/sunbird-util/sunbird-platform-core/common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
@@ -1142,6 +1142,6 @@ public final class JsonKey {
   public static final String START_DATE_BATCH = "start_date";
   public static final String END_DATE_BATCH = "end_date";
   public static final String ADD_EXTRA_HOURS_MINS = "addExtraHrsAndMins.start_date_end_date";
-
+  public static final String STANDALONE_ASSESSMENT ="Standalone Assessment";
   private JsonKey() {}
 }
